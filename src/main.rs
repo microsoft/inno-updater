bf93d0e18b2d469374c08e18e1081640779dd735
Joao Moreno <mail@joaomoreno.com>
2017-12-28 23:43:58 +0000

    play

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+#[derive(Debug)]
+struct Rectangle {
+	width: u32,
+	height: u32,
+}
+
+impl Rectangle {
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}
+}
+
 fn main() {
-    println!("Hello, world!");
+	let rect1 = Rectangle {
+		width: 30,
+		height: 50,
+	};
+
+	println!(
+		"The area of the rectangle {:?} is {} square pixels.",
+		rect1,
+		rect1.area()
+	);
 }
