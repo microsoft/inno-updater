8a36bcca10cea5f34eed20aef1a326e7fbbe213a
Joao Moreno <jomo@microsoft.com>
2018-01-12 08:20:12 +0100

    gui demo

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate byteorder;
 extern crate crc;
+#[macro_use] extern crate native_windows_gui as nwg;
+extern crate user32;
+use nwg::{Event, Ui, simple_message, fatal_message, dispatch_events};
 
 mod blockio;
 mod strings;
@@ -7,6 +10,8 @@ mod model;
 
 use std::fs;
 use std::io;
+use std::thread;
+use std::time;
 use std::io::prelude::*;
 use std::vec::Vec;
 use std::collections::HashMap;
@@ -28,7 +33,7 @@ fn print_statistics(recs: &[model::FileRec]) {
 	}
 }
 
-fn main() {
+fn patch() {
 	let input_file = fs::File::open("unins000.dat").expect("file not found");
 	let mut input = io::BufReader::new(input_file);
 
@@ -64,4 +69,128 @@ fn main() {
 	writer.flush().expect("flush");
 	// println!("{:?}", header);
  // print_statistics(&recs);
+
+}
+
+#[derive(Debug, Clone, Hash)]
+pub enum AppId {
+    // Controls
+    MainWindow,
+    // NameInput,
+		ProgressBar,
+    HelloButton,
+    Label(u8),   // Ids for static controls that won't be referenced in the Ui logic can be shortened this way.
+
+    // Events
+    SayHello,
+
+    // Resources
+    MainFont,
+    TextFont
+}
+
+use AppId::*; // Shortcut
+
+pub fn setup_ui(ui: &Ui<AppId>) -> Result<(), nwg::Error> {
+
+    // nwg_font!(family="Arial"; size=27)
+    let f1 = nwg_font!(family="Segoe UI"; size=27);
+
+    // nwg_font!(family="Arial"; size=17)
+    let f2 = nwg::FontT {
+        family: "Segoe UI", size: 17,
+        weight: nwg::constants::FONT_WEIGHT_NORMAL,
+        decoration: nwg::constants::FONT_DECO_NORMAL,
+    };
+
+    // nwg_window!( title="Template Example"; size=(280, 105))
+    let window = nwg::WindowT {
+        title: "No template",
+        position: (100, 100), size: (280, 105),
+        resizable: false, visible: true, disabled: false,
+        exit_on_close: true
+    };
+
+    // nwg_label!( parent="MainWindow"; [...] font=Some("TextFont") )
+    let label = nwg::LabelT {
+        text: "Your Name: ",
+        position: (5,15), size: (80, 25),
+        visible: true, disabled: false,
+        align: nwg::constants::HTextAlign::Left,
+        parent: MainWindow, font: Some(TextFont)
+    };
+
+    // nwg_textinput!( parent="MainWindow"; [..] font=Some("TextFont") )
+    // let tedit = nwg::TextInputT::<_, &'static str, _> {
+    //     text: "",
+    //     position: (85,13), size: (185,22),
+    //     visible: true, disabled: false, readonly: false, password: false,
+    //     limit: 32_767, placeholder: None,
+    //     parent: MainWindow, font: Some(TextFont)
+    // };
+
+		let pbar = nwg_progressbar!(parent=MainWindow; position=(85,13); size=(185,22));
+
+    // nwg_button!( parent="MainWindow"; [..] font=Some("MainFont") )
+    let hellbtn = nwg::ButtonT {
+        text: "Hello World!",
+        position: (5, 45), size: (270, 50),
+        visible: true, disabled: false,
+        parent: MainWindow, font: Some(MainFont)
+    };
+
+    // resources:
+    ui.pack_resource(&MainFont, f1);
+    ui.pack_resource(&TextFont, f2);
+
+    // controls:
+    ui.pack_control(&MainWindow, window);
+    ui.pack_control(&Label(0), label);
+    // ui.pack_control(&NameInput, tedit);
+    ui.pack_control(&ProgressBar, pbar);
+    ui.pack_control(&HelloButton, hellbtn);
+
+    // events:
+    ui.bind(&HelloButton, &SayHello, Event::Click, |ui,_,_,_| {
+        if let Ok(pbar) = ui.get::<nwg::ProgressBar>(&ProgressBar) {
+            pbar.set_value(pbar.get_value()+10);
+        } else {
+            panic!()
+        }
+    });
+
+    ui.commit()
+}
+
+fn show_window() {
+    let app: Ui<AppId>;
+
+    match Ui::new() {
+        Ok(_app) => { app = _app; },
+        Err(e) => { fatal_message("Fatal Error", &format!("{:?}", e) ); }
+    }
+
+    if let Err(e) = setup_ui(&app) {
+        fatal_message("Fatal Error", &format!("{:?}", e));
+    }
+
+		let pbarHandle = app.handle_of(&ProgressBar).expect("pbar handle");
+
+		thread::spawn(|| {
+			// thread::sleep(time::Duration::from_millis(1000));
+			// appRef.has_id(&ProgressBar);
+			// let pbar = app.get::<nwg::ProgressBar>(&ProgressBar).expect("pbar");
+
+			for i in 0..100 {
+				// pbar.set_value(i);
+				user32::PostMessageW(pbarHandle, PBM_SETPOS, i, 0);
+				thread::sleep(time::Duration::from_millis(100));
+			}
+    });
+
+    dispatch_events();
+}
+
+fn main() {
+	show_window();
 }
