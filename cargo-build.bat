5f2242454440757facdd9645d278f8e81fcb3c3d
Joao Moreno <joao.moreno@microsoft.com>
2018-01-23 16:25:50 +0100

    always build 32 bits

--- cargo-build.bat
+++ cargo-build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 SET cargoargs=
-IF "%~1"=="--release" SET cargoargs=--release
+IF "%~1"=="--release" SET cargoargs=--release --target="i686-pc-windows-msvc"
 
 cargo build %cargoargs%
 CALL .\enable-visual-styles.bat %~1
--- enable-visual-styles.bat
+++ enable-visual-styles.bat
@@ -1,6 +1,6 @@
 @echo off
 
 SET target=debug
-IF "%~1"=="--release" SET target=release
+IF "%~1"=="--release" SET target=i686-pc-windows-msvc\release
 
 .\tools\mt.exe -nologo -manifest main.manifest -outputresource:"target\%target%\inno_updater.exe;#1"
