320c2fe629dc6f136174910d374a805cc735f15a
Joao Moreno <jomo@microsoft.com>
2018-02-26 16:05:56 +0100

    add OSSREADME.json

--- tools/legal/main.js
+++ tools/legal/main.js
@@ -2,42 +2,117 @@ const got = require('got');
 const toml = require('toml');
 const fs = require('mz/fs');
 const pall = require('p-all');
+const minimist = require('minimist');
+const githubKey = '40f569379fda89ebb6390f6b0b1e54abab5ed0bf';
 
-async function getCrateInfo(name, version) {
-	const res = await got(`https://crates.io/api/v1/crates/${name}/${version}`, { json: true });
+async function getLicenseFromAPI(repository) {
+	const res = await got(`https://api.github.com/repos/${repository}/license`, {
+		json: true,
+		auth: githubKey,
+		headers: {
+			Accept: 'application/vnd.github.v3+json'
+		}
+	});
+
+	return Buffer.from(res.body.content, 'base64').toString('utf8');
+}
+
+async function getFileFromRepository(repository, file) {
+	const res = await got(`https://raw.githubusercontent.com/${repository}/master/${file}`, { auth: githubKey });
+	return res.body;
+}
+
+async function getLicenseFromRepository(repository) {
+	try {
+		return (await getFileFromRepository(repository, 'LICENSE-MIT'));
+	} catch (err) {
+		try {
+			return (await getFileFromRepository(repository, 'LICENSE-APACHE'));
+		} catch (err) {
+			try {
+				return (await getFileFromRepository(repository, 'LICENSE'));
+			} catch (err) {
+				return (await getFileFromRepository(repository, 'LICENSE.md'));
+			}
+		}
+	}
+}
+
+async function getLicense(repository) {
+	try {
+		return await getLicenseFromAPI(repository);
+	} catch (err) {
+		return await getLicenseFromRepository(repository);
+	}
+}
+
+async function getCrateInfo(name) {
+	const res = await got(`https://crates.io/api/v1/crates/${name}`, { json: true });
 	return res.body;
 }
 
 async function main(argv) {
-	if (argv.length < 3) {
-		throw new Error('Usage: node legal [Cargo.lock]');
+	if (argv._.length < 1) {
+		throw new Error('Usage: node legal [--ossreadme] Cargo.lock');
 	}
 
-	console.log('Checking OSS dependencies for MIT license...');
+	const ossreadme = [];
 
-	const raw = await fs.readFile(argv[2], 'utf8');
+	console.error('Checking OSS dependencies for MIT license...');
+
+	const raw = await fs.readFile(argv._[0], 'utf8');
 	const cargolock = toml.parse(raw);
 	const tasks = cargolock.package
 		.filter(pkg => pkg.name !== 'inno_updater')
 		.map(pkg => async () => {
-			const info = await getCrateInfo(pkg.name, pkg.version);
-			const isMIT = /MIT/.test(info.version.license);
+			const info = await getCrateInfo(pkg.name);
+			const versionInfo = info.versions.filter(v => v.num === pkg.version)[0];
 
-			console.log(`${info.version.crate} ${info.version.num} ${info.version.license} ${isMIT ? '✔︎' : '✖︎'}`);
+			if (argv['ossreadme']) {
+				const repositoryUrl = info.crate.repository;
+				const match = /github\.com\/([^/]+\/[^/]+)(\/|$)/.exec(repositoryUrl);
 
+				if (!match) {
+					console.error(`${pkg.name} does not live in github: ${repositoryUrl}`);
+					process.exit(1);
+				}
+
+				const repository = match[1].replace(/\.git$/, '');
+				const license = await getLicense(repository);
+
+				ossreadme.push({
+					name: repository,
+					version: versionInfo.num,
+					repositoryUrl,
+					licenseDetail: license.trim().split(/\r?\n/g),
+					isProd: true
+				})
+			}
+
+			const isMIT = /MIT/.test(versionInfo.license);
+
+			console.error(`${versionInfo.crate} ${versionInfo.num} ${versionInfo.license} ${isMIT ? '✔︎' : '✖︎'}`);
 			return isMIT;
 		});
 
 	const areMIT = await pall(tasks, { concurrency: 10 });
-	const ok = areMIT.reduce((r, v) => r && v, true);
+	const allAreMIT = areMIT.reduce((r, v) => r && v, true);
 
-	if (!ok) {
-		console.error('Some dependencies are not MIT!');
+	if (!allAreMIT) {
+		throw new Error('Some dependencies are not MIT!');
 	}
 
-	return ok ? 0 : 1;
+	if (argv['ossreadme']) {
+		console.log(JSON.stringify(ossreadme, null, '\t'));
+	}
+
+	return;
 }
 
-main(process.argv)
+const opts = {
+	boolean: 'ossreadme'
+};
+
+main(minimist(process.argv.slice(2), opts))
 	.catch(err => { console.error(err.message); return 1; })
 	.then(result => process.exit(result));
