7184568090f9eef8c33655241b0d1cffd4b3741f
Joao Moreno <joao.moreno@microsoft.com>
2018-02-27 15:18:05 +0100

    add icon

--- cargo-build.bat
+++ cargo-build.bat
@@ -4,4 +4,9 @@ SET cargoargs=
 IF "%~1"=="--release" SET cargoargs=--release --target="i686-pc-windows-msvc"
 
 cargo build %cargoargs%
-CALL .\enable-visual-styles.bat %~1
+
+SET target=debug
+IF "%~1"=="--release" SET target=i686-pc-windows-msvc\release
+
+.\tools\mt.exe -nologo -manifest resources\main.manifest -outputresource:"target\%target%\inno_updater.exe;#1"
+.\tools\rcedit.exe target\%target%\inno_updater.exe --set-icon resources\code.ico
--- enable-visual-styles.bat
+++ enable-visual-styles.bat
@@ -3,4 +3,4 @@
 SET target=debug
 IF "%~1"=="--release" SET target=i686-pc-windows-msvc\release
 
-.\tools\mt.exe -nologo -manifest main.manifest -outputresource:"target\%target%\inno_updater.exe;#1"
+.\tools\mt.exe -nologo -manifest resources\main.manifest -outputresource:"target\%target%\inno_updater.exe;#1"
