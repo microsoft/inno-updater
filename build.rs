190b154a5eaf391faac1ce3a16c6f3f6ffe7db3e
Charles Milette <me@charlesmilette.net>
2018-06-10 12:17:50 -0400

    Make indent style consistent

--- build.rs
+++ build.rs
@@ -1,19 +1,19 @@
 use std::process::Command;
 
 fn main() {
-    let mut rc = Command::new(".\\tools\\rc.exe")
-                         .arg("/r")
-                         .arg("/fo")
-                         .arg(".\\resources\\resources.lib")
-                         .arg(".\\resources\\resources.rc")
-                         .spawn()
-                         .expect("Failed to spawn resource compiler");
+	let mut rc = Command::new(".\\tools\\rc.exe")
+	                     .arg("/r")
+	                     .arg("/fo")
+	                     .arg(".\\resources\\resources.lib")
+	                     .arg(".\\resources\\resources.rc")
+	                     .spawn()
+	                     .expect("Failed to spawn resource compiler");
 
-    let ecode = rc
-                .wait()
-                .expect("Failed to wait on resource compiler");
+	let ecode = rc
+	            .wait()
+	            .expect("Failed to wait on resource compiler");
 
-    assert!(ecode.success(), "Resource compiler failed");
+	assert!(ecode.success(), "Resource compiler failed");
 
-    println!("cargo:rustc-link-lib=static=.\\resources\\resources");
+	println!("cargo:rustc-link-lib=static=.\\resources\\resources");
 }
