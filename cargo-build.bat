48ce0758f7d16f9b5eef2bec3f8577e202ff81cf
João Moreno <joao.moreno@microsoft.com>
2021-10-27 16:02:51 +0200

    remove unnecessary scripts

--- cargo-build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET cargoargs=
-IF "%~1"=="--release" SET cargoargs=--release --target="i686-pc-windows-msvc"
-
-cargo build %cargoargs%
-
-SET target=debug
-IF "%~1"=="--release" SET target=i686-pc-windows-msvc\release
--- cargo-run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-SET target=debug
-IF "%~1"=="--release" SET target=release
-
-CALL .\cargo-build.bat %~1
-.\target\%target%\inno_updater.exe %*
